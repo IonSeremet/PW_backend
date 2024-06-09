6d597d1f7619d727baca9c9a6cb2b2ebbf9d8252
IonSeremet <ionseremet@gmail.com>
2024-06-09 23:09:22 +0300

    Finalised the project

--- symfony-6-rest-api/src/Controller/ProductsController.php
+++ symfony-6-rest-api/src/Controller/ProductsController.php
@@ -1,57 +1,57 @@
 <?php
- 
+
 namespace App\Controller;
- 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Products;
- 
+
 #[Route('/products')]
 class ProductsController extends AbstractController
 {
-    #[Route('', methods:['get'] )]
+    #[Route('', methods: ['get'])]
     public function index(ManagerRegistry $doctrine): JsonResponse
     {
         $users = $doctrine
             ->getRepository(Products::class)
             ->findAll();
-   
-        $data = [];
-   
-        foreach ($users as $user) {
-           $data[] = [
-            'id' => $user->getId(),
-            'name' => $user->getName(),
-            'price' => $user->getPrice(),
-            'desription' => $user->getDesription(),
-            'available_quantity' => $user->getAvailableQuantity(),
-            'category_id' => $user->getCategoryId(),
 
-           ];
+        $data = [];
+
+        foreach ($users as $user) {
+            $data[] = [
+                'id' => $user->getId(),
+                'name' => $user->getName(),
+                'price' => $user->getPrice(),
+                'desription' => $user->getDesription(),
+                'available_quantity' => $user->getAvailableQuantity(),
+                'category_id' => $user->getCategoryId(),
+
+            ];
         }
-   
+
         return $this->json($data);
     }
- 
- 
-    #[Route('', methods:['POST'] )]
+
+
+    #[Route('', methods: ['post'])]
     public function create(ManagerRegistry $doctrine, Request $request): JsonResponse
     {
         $entityManager = $doctrine->getManager();
-   
+
         $user = new Products();
         $user->setName($request->request->get('name'));
         $user->setPrice($request->request->get('price'));
         $user->setDesription($request->request->get('desription'));
         $user->setCategoryId($request->request->get('category_id'));
         $user->setAvailableQuantity($request->request->get('available_quantity'));
-   
+
         $entityManager->persist($user);
         $entityManager->flush();
-   
+
         $data =  [
             'id' => $user->getId(),
             'name' => $user->getName(),
@@ -60,21 +60,21 @@ class ProductsController extends AbstractController
             'available_quantity' => $user->getAvailableQuantity(),
             'category_id' => $user->getCategoryId(),
         ];
-           
+
         return $this->json($data);
     }
- 
- 
-    #[Route('/{id}', methods:['get'] )]
+
+
+    #[Route('/{id}', methods: ['get'])]
     public function show(ManagerRegistry $doctrine, int $id): JsonResponse
     {
         $user = $doctrine->getRepository(Products::class)->find($id);
-   
+
         if (!$user) {
-   
+
             return $this->json('No user found for id ' . $id, 404);
         }
-   
+
         $data =  [
             'id' => $user->getId(),
             'name' => $user->getName(),
@@ -83,27 +83,27 @@ class ProductsController extends AbstractController
             'available_quantity' => $user->getAvailableQuantity(),
             'category_id' => $user->getCategoryId(),
         ];
-           
+
         return $this->json($data);
     }
- 
-    #[Route('/{id}', methods:['put', 'patch'] )]
+
+    #[Route('/{id}', methods: ['put', 'patch'])]
     public function update(ManagerRegistry $doctrine, Request $request, int $id): JsonResponse
     {
         $entityManager = $doctrine->getManager();
         $user = $entityManager->getRepository(Products::class)->find($id);
-   
+
         if (!$user) {
             return $this->json('No user found for id' . $id, 404);
         }
-   
+
         $user->setName($request->request->get('name'));
         $user->setPrice($request->request->get('price'));
         $user->setDesription($request->request->get('desription'));
         $user->setCategoryId($request->request->get('category_id'));
         $user->setAvailableQuantity($request->request->get('available_quantity'));
         $entityManager->flush();
-   
+
         $data =  [
             'id' => $user->getId(),
             'name' => $user->getName(),
@@ -112,23 +112,30 @@ class ProductsController extends AbstractController
             'available_quantity' => $user->getAvailableQuantity(),
             'category_id' => $user->getCategoryId(),
         ];
-           
+
         return $this->json($data);
     }
- 
-    #[Route('/{id}', methods:['delete'] )]
-    public function delete(ManagerRegistry $doctrine, int $id): JsonResponse
+
+
+    #[Route('/{name}', methods: ['delete'])]
+    public function deleteProductByName(ManagerRegistry $doctrine, string $name): JsonResponse
     {
         $entityManager = $doctrine->getManager();
-        $user = $entityManager->getRepository(Products::class)->find($id);
-   
-        if (!$user) {
-            return $this->json('No user found for id' . $id, 404);
+        $productRepository = $entityManager->getRepository(Products::class);
+
+        // Find product by name
+        $product = $productRepository->findOneBy(['name' => $name]);
+
+        if (!$product) {
+            return $this->json('No product found with name ' . $name, 404);
         }
-   
-        $entityManager->remove($user);
+
+        $productId = $product->getId();
+
+        // Delete the product
+        $entityManager->remove($product);
         $entityManager->flush();
-   
-        return $this->json('Deleted a user successfully with id ' . $id);
+
+        return $this->json('Deleted a product successfully with id ' . $productId);
     }
 }
